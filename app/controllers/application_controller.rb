110fc58636b27f4695a3152d582c74126335cc41
lbhasemeyer <lbhasemeyer@gmail.com>
2014-12-02 10:17:35 -0700

    added 404 message to pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  class AccessDenied < StandardError
+  end
+
+  rescue_from AccessDenied, with: :elephants
+
+  def elephants
+    render file: "public/404", status: 404, layout: false
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,7 @@
 class PagesController < ApplicationController
 
+  def index
+    raise AccessDenied
+  end
+
 end
